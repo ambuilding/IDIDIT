0a236637de6b9637f4aa1b99efa23524dfb0eda1
ambuilding <explorewo@gmail.com>
2017-10-09 12:45:37 +0800

    remoteFilter
    Fetch data from server by the matched string. arraySlice/limit, delay,

--- atJS/users.php
+++ atJS/users.php
@@ -2,4 +2,15 @@
 
 header('Content-Type: application/json');
 
-echo json_encode(['Peter', 'Tom', 'Anne']);
+$usernames = ['Peter', 'Tom', 'Anne', 'Andy', 'Tank'];
+
+$results = array_values(array_filter($usernames, function ($name) {
+	return strpos($name, $_GET['q']) === 0;
+}));
+
+array_slice($results, 0, 5);
+
+echo json_encode($results);
+
+// php users.php
+// php-cgi users.php q=An
